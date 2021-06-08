bd29d3099ccec38c3832e27a5b293952c8f8b1d1
12W300 <78951373+12W300@users.noreply.github.com>
2021-06-08 12:58:24 +0300

    Delete ind1.py

--- ind1.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-
-if __name__ == '__main__':
-    st = input('Введите текст ')
-    count = st.count('нн')
-    for i in range(count):
-        print('нн')
