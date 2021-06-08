0cb113048388ec73fe5301199de94cd221832506
12W300 <78951373+12W300@users.noreply.github.com>
2021-06-08 12:58:38 +0300

    Add files via upload

---
+++ ind1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+if __name__ == '__main__':
+    st = input('Введите текст ')
+    count = st.count('нн')
+    i = 0
+    while i != count:
+        i += 1
+        print('нн')
