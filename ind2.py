69d2bed6599a7b66bf71260d71484bf130fef0b1
12W300 <78951373+12W300@users.noreply.github.com>
2021-06-08 19:42:52 +0300

    Delete ind2.py

--- ind2.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-
-if __name__ == '__main__':
-    s = input('Введите текст ')
-    s = s.replace('чя', 'ча')
-    s = s.replace('щя', 'ща')
-    print(s)
